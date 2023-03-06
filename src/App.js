802aecd37300e2edbf1f06c2369967a16095b9c4
andipstha <sandip.stha120@gmail.com>
2023-03-06 23:53:30 +0545

    my chat deployed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'f34e4d9c-6de8-49ae-9062-8a76ed507c82';
+
 const App = () => {
-  return(
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
     <ChatEngine
-        height="100vh"
-        projectID="f34e4d9c-6de8-49ae-9062-8a76ed507c82"
-        userName="andipstha"
-        userSecret="1234"
-        renderChatFeed={(chatAppProps) => <ChatFeed{...chatAppProps}/>}
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
+  );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
